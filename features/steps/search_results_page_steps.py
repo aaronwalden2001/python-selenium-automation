c05f30882bd902da7e9300718b37b85376e1c42c
svetlannnka <svetlana.levinsohn@gmail.com>
2026-01-03 12:06:30 -0800

    Lesson 4

---
+++ features/steps/search_results_page_steps.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+from behave import given, when, then
+
+SEARCH_RESULTS_TEXT = (By.XPATH, "//div[contains(@class,'styles_listingPageResultsCount')]")
+
+
+@then('Search results for {expected_product} are shown')
+def verify_search_results(context, expected_product):
+    actual_text = context.driver.find_element(*SEARCH_RESULTS_TEXT).text
+    assert expected_product in actual_text, f'Expected text {expected_product} not in actual text {actual_text}'
+
--- features/tests/cart.feature
+++ features/tests/cart.feature
@@ -1,6 +1,6 @@
-# Created by lana at 12/27/25
 Feature: Test cases for Cart
-  # Enter feature description here
 
-#  Scenario: # Enter scenario name here
-    # Enter steps here
+  Scenario: User can see Empty Cart message
+    Given Open Target main page
+    When Click on cart icon
+    Then Empty Cart message is shown
