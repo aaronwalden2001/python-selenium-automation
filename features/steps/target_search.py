b6a8b11e209bafe1687cf00cde212e46ec38bc7c
Aaron Walden <walden.aaron01@outlook.com>
2026-01-06 19:02:01 -0500

    HW3: BDD tests and optimized locators

--- features/steps/target_search.py
+++ features/steps/target_search.py
@@ -9,14 +9,47 @@ def open_main(context):
 
 @when('Search for tea')
 def search_product(context):
-  #  context.driver.find_element(By.ID, 'search').send_keys('tea')
-     context.driver.find_element(By.CSS_SELECTOR,"").click()
+  context.driver.find_element(By.ID, 'search').send_keys('tea')
+  sleep(5)
+
+@then('Search results for tea are shown')
+def search_results(context):
+    context.driver.find_element(By.XPATH, "//button[@aria-label = 'search']").click()
+    sleep(5)
 
 @when('Account button clicked')
 def click_account_button(context):
     context.driver.find_element(By.XPATH,"//a[aria-label='Account, sign in']").click()
-    sleep(1)
+    sleep(5)
 @then('Signin page shown')
 def sign_in_page(context):
     context.driver.find_element(By.XPATH,"//button[@class='styles_ndsBaseButton__4Gp2_ styles_md__Lvk4a styles_fullWidth__gA46D styles_ndsButton__XOOOH styles_md__Yc3tr styles_filled___MOAP h-margin-t-x2 h-margin-b-default']").click()
+
+    sleep(5)
+
+#Cart functionality
+@when('user clicks on cart icon')
+def click_cart_icon(context):
+    context.driver.find_element(By.CSS_SELECTOR,"a.styles_ndsLink__GUaai.styles_onLight__QKcK7.sc-b8685e67-1.sc-d9c75939-0.grtDbE.bgAxjv").click()
+    sleep(5)
+
+@then('user sees cart empty message')
+def cart_empty_message(context):
+    context.driver.find_element(By.XPATH, "//h1[@class= 'styles_ndsHeading__phw6r styles_fontSize1__OL7f3 styles_x2Margin__ZKMpf']")
+    sleep(5)
+
+#logged out
+@when('user clicks Sign In button')
+def click_sign_in(context):
+    context.driver.find_element(By.ID,'account-sign-in').click()
+    sleep(5)
+
+@when('user clicks Sign In from side navigation')
+def second_sign_in_sidemenu(context):
+    context.driver.find_element(By.XPATH, "//button[@class = 'styles_ndsBaseButton__4Gp2_ styles_md__Lvk4a styles_fullWidth__gA46D styles_ndsButton__XOOOH styles_md__Yc3tr styles_filled___MOAP h-margin-t-x2 h-margin-b-default']").click()
+    sleep(5)
+
+@then('Sign In form is displayed')
+def sign_in_form(context):
+    context.driver.find_element(By.XPATH, "//div[@class = 'sc-e288a325-2 cwgIAc'")
     sleep(5)
--- features/tests/target_search.feature
+++ features/tests/target_search.feature
@@ -1,13 +1,23 @@
 # Created by aaronwalden at 12/27/25
-Feature: Tests for search
+Feature: Tests for search,sign in, and cart function
   Scenario: User can search for a product on Target
     Given Open Target main page
     When Search for tea
     Then Search results for tea are shown
   # Enter feature description here
 
- Feature: Tests for Signin
-  Scenario: User Sign into Target
+#Feature: Cart functionality
+
+  Scenario: User sees empty cart message
     Given Open Target main page
-    When Account button clicked
-    Then Signin page shown
+    When user clicks on cart icon
+    Then user sees cart empty message
+
+
+#Feature: Sign In navigation
+
+  Scenario: Logged out user can navigate to Sign In page
+    Given Open Target main page
+    When user clicks Sign In button
+    And user clicks Sign In from side navigation
+    Then Sign In form is displayed
